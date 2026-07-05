1ccc1de102c615be0aa6c92c17ea144e71fb1333
Joti <hasnahenajoti1414@gmail.com>
2026-07-05 11:30:04 +0600

    feat: add seed data and localStorage persistence hook

---
+++ src/data/movies.js
@@ -0,0 +1,40 @@
+const initialMovies = [
+  {
+    id: "m1",
+    title: "Interstellar",
+    genre: "Sci-Fi",
+    year: 2014,
+    posterUrl:
+      "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+    watched: true,
+  },
+  {
+    id: "m2",
+    title: "Parasite",
+    genre: "Thriller",
+    year: 2019,
+    posterUrl:
+      "https://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg",
+    watched: true,
+  },
+  {
+    id: "m3",
+    title: "Dune: Part Two",
+    genre: "Sci-Fi",
+    year: 2024,
+    posterUrl:
+      "https://image.tmdb.org/t/p/w500/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg",
+    watched: false,
+  },
+  {
+    id: "m4",
+    title: "The Grand Budapest Hotel",
+    genre: "Comedy",
+    year: 2014,
+    posterUrl:
+      "https://image.tmdb.org/t/p/w500/eWdyYQreja6JGCzqHWXpWHDrrPo.jpg",
+    watched: false,
+  },
+];
+
+export default initialMovies;
